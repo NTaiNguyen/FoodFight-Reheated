635f877292a43bf97bd20253a5c20f41a18eebf3
Tai Nguyen <147784810+NTaiNguyen@users.noreply.github.com>
2025-10-06 16:43:15 -0400

    Fixed animations

--- Assets/Scripts/MovementScript.cs
+++ Assets/Scripts/MovementScript.cs
@@ -18,8 +18,8 @@ public class MovementScript : MonoBehaviour
 {
     private Rigidbody2D rb;
     private AnimationController animControl;
-    private BoxCollider2D box;
-    public BoxCollider2D crouchBox;
+    [SerializeField] private BoxCollider2D box;
+    [SerializeField] private BoxCollider2D crouchBox;
     // Stores the base size and offset of the character collider
     private Vector2 defaultSize, defaultOffset;
     private bool isGrounded;
@@ -67,32 +67,24 @@ public class MovementScript : MonoBehaviour
     private void UpdateState() {
         if (!isGrounded) {
             sMove = rb.linearVelocity.y > 0 ? MoveState.JUMP : MoveState.FALL;
-            RestoreColliderSize();
         }
         else {
             if (Input.GetKey(KeyCode.S)) {
                 sMove = MoveState.CROUCH;
-                // Changes collider to be shorter for the 
-                box.size = new Vector2(defaultSize.x, defaultSize.y * 0.5f);
-                box.offset = new Vector2(defaultOffset.x, defaultOffset.y - defaultOffset.y * 0.25f);
+                rb.linearVelocity = Vector2.zero;
+                
             }
             else if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0) {
                 sMove = MoveState.WALK;
-                RestoreColliderSize();
             }
             else {
                 sMove = MoveState.STAND;
-                RestoreColliderSize();
             }
         }
         Debug.Log("sMove = " + sMove + " | sAct = " + sAct);
 
     }
-    void RestoreColliderSize() {
-        // Sets collider back to the standing size
-        box.size = defaultSize;
-        box.offset = defaultOffset;
-    }
+
 
     void OnCollisionEnter2D(Collision2D collision){
         if(collision.gameObject.CompareTag("Ground")){
