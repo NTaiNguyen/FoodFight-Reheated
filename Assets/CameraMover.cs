45345d21fcbb7d316d5aa43ebf21aac56ed71a4f
cylerspace <ckern5@uncc.edu>
2025-10-14 16:43:57 -0400

    Started on the menus

---
+++ Assets/CameraMover.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+
+public class CameraMover : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Transform targetPosition;
+
+    void Update()
+    {
+        if (targetPosition != null)
+        {
+            transform.position = Vector3.Lerp(transform.position, targetPosition.position, Time.deltaTime * moveSpeed
+            );
+        }
+    }
+
+    public void MoveTo(Transform newTarget)
+    {
+        targetPosition = newTarget;
+    }
+}
